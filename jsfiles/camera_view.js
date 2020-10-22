1df5918e252146c037fe723c638617fb4321c663
bachinaram <55042314+bachinaram@users.noreply.github.com>
2020-10-22 14:41:24 +0530

    Update camera_view.js
    adding test positional and rotational code

--- jsfiles/camera_view.js
+++ jsfiles/camera_view.js
@@ -1,12 +1,14 @@
 AFRAME.registerComponent('rotation-reader', {
   tick: function () {
     
-    console.log(this.el.object3D.position.x);
+    //console.log(this.el.object3D.position.x);
     myPosition_x = this.el.object3D.position.x
     //y always constant which is 1.6
     myPosition_z = this.el.object3D.position.z
     //console.log(this.el.object3D.rotation);
     // `position` is a three.js Vector3. this.el is element and object3D in 3js get the position/rotation which 
     //contains x,y,z
+    myBeta = this.el.object3D.rotation.y;
+    myLength = 5;//this is an assumption that my left and right wall depth is 5
   }
 });
