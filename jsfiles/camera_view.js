1cc082381a49c1ed8589c67dc164caa7c1c5290a
Ram Bachina <ram.pratap@students.iiit.ac.in>
2020-10-31 00:07:05 +0530

    getValuesfrompathgen function logic

--- jsfiles/camera_view.js
+++ jsfiles/camera_view.js
@@ -2,23 +2,33 @@
 
 AFRAME.registerComponent('rotation-reader', {
   tick: function () {
-    //console.log(this.el.object3D.position.x);
-    myPosition_x = this.el.object3D.position.x
+
+    myStartPosition_x = this.el.object3D.position.x
     //y always constant which is 1.6
-    myPosition_z = this.el.object3D.position.z
-    //console.log(this.el.object3D.rotation);
+    myStartPosition_z = this.el.object3D.position.z
     // `position` is a three.js Vector3. this.el is element and object3D in 3js get the position/rotation which 
     //contains x,y,z
-    myBeta = this.el.object3D.rotation.y;
-    myLength = 5;//this is an assumption that my left and right wall depth is 5
-    //console.log(myPosition_x,myPosition_z,myBeta,myLength)
-    //if(((myPosition_x<2.7)&&(myPosition_x>-2.7)) && (myPosition_z<-2.7))
-    //getValuesFromPathGen(myPosition_x,myPosition_z,myBeta,myLength)
+    myStartBeta = this.el.object3D.rotation.y;
+    myLength = 2;//this is an assumption that my left and right wall depth is 5
+    camera_left_x=-2.7, camera_right_x=2.7, camera_forward_z=-7.2
+    
+    //console.log(myStartPosition_x,myStartPosition_z,myBeta,myLength)
+    if(((myStartPosition_x<camera_right_x)&&(myStartPosition_x>camera_left_x)) && (myStartPosition_z<camera_forward_z)){
+      let new_values = getValuesFromPathGen(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
+      new_position_x=new_values.new_x_point
+      new_position_z=new_values.new_z_point
+      new_rotation_z=new_values.new_beta_point
+      //need to write code here for finding out the mid point of so that we can caliculate left and right of thatt mid point
+      //and also from the new room we can get camera position ahead point whenit reaches it must call
+      //getVlaues from pathgen function and do the recursion
+
+    }
   }
 });
 
-
-
+//this function is called recursively once it reaches end of room,
+//and it returns new values of x,z position and beta wrt y rotation
+//
 function getValuesFromPathGen(myPosition_x,myPosition_z,myBeta,myLength){
   var apiJson={}
   const req = new XMLHttpRequest();
@@ -28,9 +38,16 @@ function getValuesFromPathGen(myPosition_x,myPosition_z,myBeta,myLength){
   req.open('GET', myUrl);
   req.onload = () => {
     apiJson = req.response;
-    console.log(apiJson);
+    //console.log(apiJson.new_beta);
   };
   req.send();
-  console.log(apiJson);
+  new_x_point = apiJson.new_point[0];
+  new_z_point = apiJson.new_point[1];
+  new_beta_point = apiJson.new_beta;
+  return {
+    new_x_point,
+    new_z_point,
+    new_beta_point
+  };
 }
 
