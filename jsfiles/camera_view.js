96470a37bdaf6435710d72a6793c1b644b6638c5
Ram Bachina <ram.pratap@students.iiit.ac.in>
2020-10-31 10:38:24 +0530

    change in code

--- jsfiles/camera_view.js
+++ jsfiles/camera_view.js
@@ -11,13 +11,14 @@ AFRAME.registerComponent('rotation-reader', {
     myStartBeta = this.el.object3D.rotation.y;
     myLength = 2;//this is an assumption that my left and right wall depth is 5
     camera_left_x=-2.7, camera_right_x=2.7, camera_forward_z=-7.2
-    
-    //console.log(myStartPosition_x,myStartPosition_z,myBeta,myLength)
+    //console.log(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
     if(((myStartPosition_x<camera_right_x)&&(myStartPosition_x>camera_left_x)) && (myStartPosition_z<camera_forward_z)){
-      let new_values = getValuesFromPathGen(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
-      new_position_x=new_values.new_x_point
-      new_position_z=new_values.new_z_point
-      new_rotation_z=new_values.new_beta_point
+      var new_values = getValuesFromPathGen(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
+      console.log(new_values)
+      //new_position_x=new_values.new_x_point
+      //console.log(new_position_x)
+      //new_position_z=new_values.new_z_point
+      //new_rotation_z=new_values.new_beta_point
       //need to write code here for finding out the mid point of so that we can caliculate left and right of thatt mid point
       //and also from the new room we can get camera position ahead point whenit reaches it must call
       //getVlaues from pathgen function and do the recursion
@@ -30,24 +31,26 @@ AFRAME.registerComponent('rotation-reader', {
 //and it returns new values of x,z position and beta wrt y rotation
 //
 function getValuesFromPathGen(myPosition_x,myPosition_z,myBeta,myLength){
+  var new_x_point,new_z_point,new_beta_point
   var apiJson={}
   const req = new XMLHttpRequest();
   req.responseType = 'json';
   var myUrl = `http://pathgen.herokuapp.com/newpoint?x=${myPosition_x}&z=${myPosition_z}&beta=${myBeta}&length=1.23&gennewbeta=1`
   //req.open('GET', 'http://pathgen.herokuapp.com/newpoint?x=0.2&z=1.3&beta=3.132&length=1.23&gennewbeta=1');
-  req.open('GET', myUrl);
-  req.onload = () => {
+  req.open('GET', myUrl, true);
+  apiJson = req.onload = () => {
     apiJson = req.response;
-    //console.log(apiJson.new_beta);
+    console.log(apiJson)
+    //console.log(apiJson)
+    /*new_beta_point = apiJson.new_beta;
+    new_x_point = apiJson.new_point[0];
+    new_z_point = apiJson.new_point[1];
+    return {
+      new_x_point,
+      new_z_point,
+      new_beta_point
+    };*/
   };
   req.send();
-  new_x_point = apiJson.new_point[0];
-  new_z_point = apiJson.new_point[1];
-  new_beta_point = apiJson.new_beta;
-  return {
-    new_x_point,
-    new_z_point,
-    new_beta_point
-  };
 }
 
