dfee2e933bbeac04b8e3cd9333c75c9e4ff465a6
Ram Bachina <ram.pratap@students.iiit.ac.in>
2020-11-21 09:12:39 +0530

    dynamic room generation using api always 180

--- jsfiles/camera_view.js
+++ jsfiles/camera_view.js
@@ -1,75 +1,198 @@
-// AFRAME.registerComponent('rotation-reader', {
-//     tick: function() {
-
-//         myStartPosition_x = this.el.object3D.position.x;
-//         //y always constant which is 1.6
-//         myStartPosition_z = this.el.object3D.position.z;
-//         // `position` is a three.js Vector3. this.el is element and object3D in 3js get the position/rotation which 
-//         //contains x,y,z
-//         myStartBeta = this.el.object3D.rotation.y;
-//         myLength = 2; //this is an assumption that my left and right wall depth is 5
-//         camera_left_x = -2.7, camera_right_x = 2.7, camera_forward_z = -7.2;
-//         //console.log(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
-//         if (((myStartPosition_x < camera_right_x) && (myStartPosition_x > camera_left_x)) && (myStartPosition_z < camera_forward_z)) {
-//             getValuesFromPathGen()
-//         }
+// AFRAME.registerComponent("clickimage", {
+//     init: function() {
+//       this.el.addEventListener("click", function(evt){
+//         console.log("testing string")
+//       })}
+//     });
+  
+//   AFRAME.registerComponent('main_fun', {
+//     init: function () {
+//       camera_left_x=-2.7, camera_right_x=2.7, camera_forward_z=-7
+//       center_z=camera_forward_z/2;
+//       myPlane=-9;
+//       closingDoor=-4;
+//       myEndWall(-10)
 //     }
-// });
-
-// //this function is called recursively once it reaches end of room,
-// //and it returns new values of x,z position and beta wrt y rotation
-// //
-// var flag = true;
-
-// function getValuesFromPathGen() {
-
-//     var beta, p1_x = 0;
-//     // var p1_y = 2.5;
-//     var p1_z = -10;
-//     if (flag == true) {
-//         beta = 2.35619; // 135 deg
-//         flag = false;
-//     } else {
-//         beta = 3.92699; // 225 deg
-//         flag = true;
+//   });
+  
+  
+//   AFRAME.registerComponent('looper-rotation-reader', {
+//     tick: function () {
+//       myStartPosition_x = this.el.object3D.position.x
+//       myStartPosition_z = this.el.object3D.position.z
+//       // `position` is a three.js Vector3. this.el is element and object3D in 3js get the position/rotation which 
+//       //contains x,y,z
+//       myStartBeta = this.el.object3D.rotation.y;
+//       //console.log(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
+//       if(((myStartPosition_x<camera_right_x)&&(myStartPosition_x>camera_left_x)) && (myStartPosition_z<camera_forward_z)){
+//         camera_forward_z=camera_forward_z-6;
+//         closingDoor=closingDoor-6;
+//         myPlane = room(camera_forward_z,myPlane,closingDoor);
+//       }
 //     }
-//     var p2_x, p2_z, p2_beta;
-//     var apiJson = {}
-//     const req = new XMLHttpRequest();
-//     req.responseType = 'json';
-//     var myUrl = `http://pathgen.herokuapp.com/newpoint?x=${p1_x}&z=${p1_z}&beta=${beta}&length=6&gennewbeta=0`
-//     req.open('GET', myUrl, true);
-//     req.onload = () => {
-//         apiJson = req.response;
-//         p2_beta = apiJson.new_beta;
-//         p2_x = apiJson.new_point[0];
-//         p2_z = apiJson.new_point[1];
-//         console.log(p2_x, p2_z, p2_beta);
-//         room(p1_x, p1_z, p2_x, p2_z, beta, flag);
-//     };
-//     req.send();
-// }
-
-
-
-// // function newValueUsageFun(apiJson) {
-// //     //console.log(apiJson)
-// //     new_beta_point = apiJson.new_beta;
-// //     new_x_point = apiJson.new_point[0];
-// //     new_z_point = apiJson.new_point[1];
-// //     console.log(new_x_point, new_z_point, new_beta_point)
-// // }
-
-
-// /* return code for function
-// return {
-//   new_x_point,
-//   new_z_point,
-//   new_beta_point
-
-//         var i;
-//   for (i = 0; i < 2; i++) {
-//     myPosition_z = this.el.object3D.position.z
-//     room()
-//   } 
-// };*/
+//   });
+  
+  
+//   function room(camera_forward_z,myPlane,closingDoor){
+    
+//     var sceneEl = document.querySelector('a-scene');
+//     //right wall
+//       var entityEl = document.createElement('a-box');
+//       entityEl.setAttribute('position', {x: 3, y: 2.5, z: camera_forward_z});
+//       entityEl.setAttribute('rotation', {x: 0, y: 360, z: 0});
+//       entityEl.setAttribute('color', '#004d80');
+//       entityEl.setAttribute('width', '0.5');
+//       entityEl.setAttribute('height', '5');
+//       entityEl.setAttribute('depth', '6');
+//       sceneEl.appendChild(entityEl);  
+  
+//       //left wall
+//       var entityEl = document.createElement('a-box');
+//       entityEl.setAttribute('position', {x: -3, y: 2.5, z: camera_forward_z});
+//       entityEl.setAttribute('rotation', {x: 360, y: 0, z: 0});
+//       entityEl.setAttribute('color', '#004d80');
+//       entityEl.setAttribute('width', '0.5');
+//       entityEl.setAttribute('height', '5');
+//       entityEl.setAttribute('depth', '6');
+//       sceneEl.appendChild(entityEl);
+  
+//       //ceiling wall
+//       var entityEl = document.createElement('a-box');
+//       entityEl.setAttribute('position', {x: 0, y: 4.75, z: camera_forward_z});
+//       entityEl.setAttribute('rotation', {x: 0, y: 0, z: 90});
+//       entityEl.setAttribute('color', '#004d80');
+//       entityEl.setAttribute('width', '0.5');
+//       entityEl.setAttribute('height', '5.6');
+//       entityEl.setAttribute('depth', '6');
+//       sceneEl.appendChild(entityEl);
+  
+//       //floor
+//       var entityEl = document.createElement('a-box');
+//       entityEl.setAttribute('material', {src: '#GalFloor'});
+//       entityEl.setAttribute('position', {x: 0, y: 0.15, z: camera_forward_z});
+//       entityEl.setAttribute('rotation', {x: 0, y: 0, z: 90});
+//       entityEl.setAttribute('width', '0.5');
+//       entityEl.setAttribute('height', '5.5');
+//       entityEl.setAttribute('depth', '6');
+//       sceneEl.appendChild(entityEl);
+  
+//       //chandeleur 
+//       var entityEl = document.createElement('a-entity');
+//       entityEl.setAttribute('gltf-model', "url(assets/images/gltf/chandeleur/chandeleur.gltf)");
+//       entityEl.setAttribute('position', {x: 0, y: 3, z: camera_forward_z});
+//       entityEl.setAttribute('rotation', {x: 360, y: 0, z: 0});
+//       entityEl.setAttribute('scale', {x: 0.04, y: 0.04, z: 0.04});
+//       sceneEl.appendChild(entityEl);
+      
+//       //security blocker to left
+//       var entityEl1 = document.createElement('a-entity');
+//       entityEl1.setAttribute('gltf-model', "url(assets/images/gltf/fence/scene.gltf)");
+//       entityEl1.setAttribute('scale', {x: 0.1, y: 0.2, z: 0.2});
+//       entityEl1.setAttribute('position', {x: -0.5, y: 0.6, z: camera_forward_z});
+//       sceneEl.appendChild(entityEl1);
+  
+//       //security blocker to right
+//       var entityEl2 = document.createElement('a-entity');
+//       entityEl2.setAttribute('gltf-model', "url(assets/images/gltf/fence/scene.gltf)");
+//       entityEl2.setAttribute('scale', {x: 0.1, y: 0.2, z: 0.2});
+//       entityEl2.setAttribute('position', {x: 2.55, y:0.6, z: camera_forward_z});
+//       sceneEl.appendChild(entityEl2);
+  
+//       myEndWall(closingDoor)
+//       myEndWall(closingDoor-6)
+      
+  
+//       for(j=0;j<3;j++){
+//         myPlane=myPlane-2;
+//         //left planes
+//         var sceneEl = document.querySelector('a-scene');
+//         var entityEl = document.createElement('a-plane');
+//         painting_no= Math.floor( Math.random() * 6);
+//         var path=`assets/images/paint${painting_no}.jpg`;
+//         entityEl.setAttribute('material',  {src: `url(${path})`});
+//         entityEl.setAttribute('position', {x: -2.73, y: 2.3, z: myPlane});
+//         entityEl.setAttribute('rotation', {x: 0, y: 90, z: 0});
+//         entityEl.setAttribute('color', 'white');
+//         entityEl.setAttribute('width', '1');
+//         entityEl.setAttribute('height', '1');
+//         entityEl.setAttribute('clickimage','');
+//         entityEl.setAttribute('animation', {
+//                   attribute: 'scale', 
+//                   dur:500, 
+//                   direction:'alternate', 
+//                   from:'1 1 1', 
+//                   to:'3 3 3', 
+//                   begin:'click', 
+//                   repeat:1});
+//         sceneEl.appendChild(entityEl);
+//         //console.log(j)
+//         //left lights zoom-image //entityEl.setAttribute('animation__opacity',{ property: "components.material.material.opacity", from: 0.1, to: 1,  dur: 1500, loop: true, dir:"alternate"});
+//         var entityEl = document.createElement('a-entity');
+//         entityEl.setAttribute('gltf-model', "url(assets/images/gltf/wallight/wallight.gltf)");
+//         entityEl.setAttribute('position', {x: -2.55, y: 2.9, z: myPlane});
+//         entityEl.setAttribute('rotation', {x: 0, y: 90, z: 0});
+//         entityEl.setAttribute('scale', {x: 0.005, y: 0.005, z: 0.005});
+//         sceneEl.appendChild(entityEl);
+  
+//         //right planes
+//         var entityEl = document.createElement('a-plane');
+//         painting_no= Math.floor( Math.random() * 6);
+//         var path=`assets/images/paint${painting_no}.jpg`;
+//         entityEl.setAttribute('material',  {src: `url(${path})`});
+//         entityEl.setAttribute('position', {x: 2.73, y: 2.3, z: myPlane});
+//         entityEl.setAttribute('rotation', {x: 0, y: -90, z: 0});
+//         entityEl.setAttribute('color', 'white');
+//         entityEl.setAttribute('width', '1');
+//         entityEl.setAttribute('height', '1');
+//         entityEl.setAttribute('clickimage','');
+//         /*
+//         entityEl.setAttribute('animation', {
+//           attribute: 'scale', 
+//           dur:500, 
+//           direction:'alternate', 
+//           from:'1 1 1', 
+//           to:'3 3 3', 
+//           begin:'click', 
+//           repeat:1});*/
+//         sceneEl.appendChild(entityEl);
+        
+//         //right lights
+//         var entityEl = document.createElement('a-entity');
+//         entityEl.setAttribute('gltf-model', "url(assets/images/gltf/wallight/wallight.gltf)");
+//         entityEl.setAttribute('position', {x: 2.55, y: 2.9, z: myPlane});
+//         entityEl.setAttribute('rotation', {x: 0, y: -90, z: 0});
+//         entityEl.setAttribute('scale', {x: 0.005, y: 0.005, z: 0.005});
+//         sceneEl.appendChild(entityEl);
+//       }
+//       return myPlane
+//   }
+  
+//   function myEndWall(closingDoor){
+//     var sceneEl = document.querySelector('a-scene');
+//     var entityEl = document.createElement('a-plane');
+//       entityEl.setAttribute('material', {opacity: 0.4, transparent: "true", side: "double"});
+//       entityEl.setAttribute('position', {x: -1.8, y: 2.45, z: closingDoor});
+//       entityEl.setAttribute('width', '2');
+//       entityEl.setAttribute('height', '4.12');
+//       entityEl.setAttribute('depth', '1');
+//       entityEl.setAttribute('color', 'white');
+//       sceneEl.appendChild(entityEl);
+  
+//       var entityEl = document.createElement('a-plane');
+//       entityEl.setAttribute('material', {opacity: 0.4, transparent: "true", side: "double"});
+//       entityEl.setAttribute('position', {x: 1.8, y: 2.45, z: closingDoor});
+//       entityEl.setAttribute('width', '2');
+//       entityEl.setAttribute('height', '4.12');
+//       entityEl.setAttribute('depth', '1');
+//       entityEl.setAttribute('color', 'white');
+//       sceneEl.appendChild(entityEl);
+  
+//       var entityEl = document.createElement('a-plane');
+//       entityEl.setAttribute('material', {opacity: 0.4, transparent: "true", side: "double"});
+//       entityEl.setAttribute('position', {x: 0, y: 3.7, z: closingDoor});
+//       entityEl.setAttribute('width', '1.6');
+//       entityEl.setAttribute('height', '1.6');
+//       entityEl.setAttribute('depth', '1');
+//       entityEl.setAttribute('color', 'white');
+//       sceneEl.appendChild(entityEl);
+//   }
--- jsfiles/dynamicScene.js
+++ jsfiles/dynamicScene.js
@@ -21,10 +21,12 @@ AFRAME.registerComponent("clickimage", {
 AFRAME.registerComponent('foo', {
     init: function() {
         camera_left_x = -2.7, camera_right_x = 2.7, camera_forward_z = -7
+        //left_wall_x=-2.7,right_wall_x=2.7
+        left_wall_z=-7,right_wall_z=-7
         center_z = camera_forward_z / 2;
         myPlane = -9;
         closingDoor = -4;
-        myEndWall(-10)
+        //myEndWall(-10)
     }
 });
 
@@ -37,10 +39,11 @@ AFRAME.registerComponent('rotation-reader', {
             //contains x,y,z
         myStartBeta = this.el.object3D.rotation.y;
         //console.log(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
-        if (((myStartPosition_x < camera_right_x) && (myStartPosition_x > camera_left_x)) && (myStartPosition_z < camera_forward_z)) {
-            // camera_forward_z=camera_forward_z-6;
+        if (((myStartPosition_x < camera_right_x) && (myStartPosition_x > camera_left_x)) && (myStartPosition_z < left_wall_z)) {
+            
+            //camera_forward_z=camera_forward_z-6;
             // closingDoor=closingDoor-6;
-            // myPlane = room(camera_forward_z,myPlane,closingDoor);
+            //myPlane = room(camera_forward_z,myPlane,closingDoor);
             getValuesFromPathGen();
         }
     }
@@ -53,10 +56,12 @@ var p1_z = -10;
 
 function getValuesFromPathGen() {
     if (flag == true) {
-        beta = 2.35619; // 135 deg
+        beta = 3.14; // 135 deg
+        //beta = 2.35619; // 135 deg
         flag = false;
     } else {
-        beta = 3.92699; // 225 deg
+        beta = 3.14; // 225 deg
+        //beta = 3.92699; // 225 deg
         flag = true;
     }
     var p2_x, p2_z, p2_beta;
@@ -92,12 +97,13 @@ function create_room(p1_x, p1_z, p2_x, p2_z, beta, flag) {
     else
         beta_degree = 45;
 
-    var left_wall_x = room_centre_x - (room_width / 2);
-    var right_wall_x = room_centre_x + (room_width / 2);
+    left_wall_x = room_centre_x - (room_width / 2);
+    right_wall_x = room_centre_x + (room_width / 2);
 
-    var left_wall_z = room_centre_z;
-    var right_wall_z = room_centre_z;
+    left_wall_z = room_centre_z;
+    right_wall_z = room_centre_z;
 
+    room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room_centre_x,room_centre_z)
     //left_wall
     // create_wall(left_wall_x, left_wall_z, beta_degree);
     //right wall
@@ -106,13 +112,16 @@ function create_room(p1_x, p1_z, p2_x, p2_z, beta, flag) {
     // similarly 3 planes on left wall and 3 on right wall
 }
 
-function room(camera_forward_z, myPlane, closingDoor) {
-
+function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room_centre_x,room_centre_z) {
+    beta_degree=180
+    console.log("leftwallx "+left_wall_x)
+    console.log("zis "+left_wall_z)
+    console.log("beta is "+beta_degree)
     var sceneEl = document.querySelector('a-scene');
     //right wall
     var entityEl = document.createElement('a-box');
-    entityEl.setAttribute('position', { x: 3, y: 2.5, z: camera_forward_z });
-    entityEl.setAttribute('rotation', { x: 0, y: 360, z: 0 });
+    entityEl.setAttribute('position', { x: right_wall_x, y: 2.5, z: right_wall_z });
+    entityEl.setAttribute('rotation', { x: 0, y: beta_degree, z: 0 });
     entityEl.setAttribute('color', '#004d80');
     entityEl.setAttribute('width', '0.5');
     entityEl.setAttribute('height', '5');
@@ -121,17 +130,17 @@ function room(camera_forward_z, myPlane, closingDoor) {
 
     //left wall
     var entityEl = document.createElement('a-box');
-    entityEl.setAttribute('position', { x: -3, y: 2.5, z: camera_forward_z });
-    entityEl.setAttribute('rotation', { x: 360, y: 0, z: 0 });
+    entityEl.setAttribute('position', { x: left_wall_x, y: 2.5, z: left_wall_z });
+    entityEl.setAttribute('rotation', { x: 0, y: beta_degree, z: 0 });
     entityEl.setAttribute('color', '#004d80');
     entityEl.setAttribute('width', '0.5');
     entityEl.setAttribute('height', '5');
     entityEl.setAttribute('depth', '6');
     sceneEl.appendChild(entityEl);
 
-    //ceiling wall
+    // //ceiling wall
     var entityEl = document.createElement('a-box');
-    entityEl.setAttribute('position', { x: 0, y: 4.75, z: camera_forward_z });
+    entityEl.setAttribute('position', { x: 0, y: 4.75, z: room_centre_z });
     entityEl.setAttribute('rotation', { x: 0, y: 0, z: 90 });
     entityEl.setAttribute('color', '#004d80');
     entityEl.setAttribute('width', '0.5');
@@ -139,113 +148,96 @@ function room(camera_forward_z, myPlane, closingDoor) {
     entityEl.setAttribute('depth', '6');
     sceneEl.appendChild(entityEl);
 
-    //floor
+    // //floor
     var entityEl = document.createElement('a-box');
     entityEl.setAttribute('material', { src: '#GalFloor' });
-    entityEl.setAttribute('position', { x: 0, y: 0.15, z: camera_forward_z });
+    entityEl.setAttribute('position', { x: 0, y: 0.15, z: room_centre_z });
     entityEl.setAttribute('rotation', { x: 0, y: 0, z: 90 });
     entityEl.setAttribute('width', '0.5');
     entityEl.setAttribute('height', '5.5');
     entityEl.setAttribute('depth', '6');
     sceneEl.appendChild(entityEl);
 
-    //chandeleur 
+    // //chandeleur 
     var entityEl = document.createElement('a-entity');
     entityEl.setAttribute('gltf-model', "url(assets/images/gltf/chandeleur/chandeleur.gltf)");
-    entityEl.setAttribute('position', { x: 0, y: 3, z: camera_forward_z });
+    entityEl.setAttribute('position', { x: 0, y: 3, z: left_wall_z });
     entityEl.setAttribute('rotation', { x: 360, y: 0, z: 0 });
     entityEl.setAttribute('scale', { x: 0.04, y: 0.04, z: 0.04 });
     sceneEl.appendChild(entityEl);
 
-    //security blocker to left
+    // //security blocker to left
     var entityEl1 = document.createElement('a-entity');
     entityEl1.setAttribute('gltf-model', "url(assets/images/gltf/fence/scene.gltf)");
     entityEl1.setAttribute('scale', { x: 0.1, y: 0.2, z: 0.2 });
-    entityEl1.setAttribute('position', { x: -0.5, y: 0.6, z: camera_forward_z });
+    entityEl1.setAttribute('position', { x: -0.5, y: 0.6, z: left_wall_z });
     sceneEl.appendChild(entityEl1);
 
-    //security blocker to right
+    // //security blocker to right
     var entityEl2 = document.createElement('a-entity');
     entityEl2.setAttribute('gltf-model', "url(assets/images/gltf/fence/scene.gltf)");
     entityEl2.setAttribute('scale', { x: 0.1, y: 0.2, z: 0.2 });
-    entityEl2.setAttribute('position', { x: 2.55, y: 0.6, z: camera_forward_z });
+    entityEl2.setAttribute('position', { x: 2.55, y: 0.6, z: left_wall_z });
     sceneEl.appendChild(entityEl2);
 
-    myEndWall(closingDoor)
-    myEndWall(closingDoor - 6)
+    myEndWall(left_wall_z)
+    myEndWall(left_wall_z - 6)
 
-
+    myPlane_z = left_wall_z+2
     for (j = 0; j < 3; j++) {
-        myPlane = myPlane - 2;
+
+        //myPlane = myPlane - 2;
         //left planes
         var sceneEl = document.querySelector('a-scene');
         var entityEl = document.createElement('a-plane');
         painting_no = Math.floor(Math.random() * 6);
         var path = `assets/images/paint${painting_no}.jpg`;
         entityEl.setAttribute('material', { src: `url(${path})` });
-        entityEl.setAttribute('position', { x: -2.73, y: 2.3, z: myPlane });
+        entityEl.setAttribute('position', { x: -2.73, y: 2.3, z: myPlane_z });
         entityEl.setAttribute('rotation', { x: 0, y: 90, z: 0 });
         entityEl.setAttribute('color', 'white');
         entityEl.setAttribute('width', '1');
         entityEl.setAttribute('height', '1');
-        entityEl.setAttribute('clickimage', '');
-        entityEl.setAttribute('animation', {
-            attribute: 'scale',
-            dur: 500,
-            direction: 'alternate',
-            from: '1 1 1',
-            to: '3 3 3',
-            begin: 'click',
-            repeat: 1
-        });
+        //entityEl.setAttribute('clickimage', '');
         sceneEl.appendChild(entityEl);
         //console.log(j)
         //left lights zoom-image //entityEl.setAttribute('animation__opacity',{ property: "components.material.material.opacity", from: 0.1, to: 1,  dur: 1500, loop: true, dir:"alternate"});
         var entityEl = document.createElement('a-entity');
         entityEl.setAttribute('gltf-model', "url(assets/images/gltf/wallight/wallight.gltf)");
-        entityEl.setAttribute('position', { x: -2.55, y: 2.9, z: myPlane });
+        entityEl.setAttribute('position', { x: -2.55, y: 2.9, z: myPlane_z });
         entityEl.setAttribute('rotation', { x: 0, y: 90, z: 0 });
         entityEl.setAttribute('scale', { x: 0.005, y: 0.005, z: 0.005 });
         sceneEl.appendChild(entityEl);
 
-        //right planes
+    //     //right planes
         var entityEl = document.createElement('a-plane');
         painting_no = Math.floor(Math.random() * 6);
         var path = `assets/images/paint${painting_no}.jpg`;
         entityEl.setAttribute('material', { src: `url(${path})` });
-        entityEl.setAttribute('position', { x: 2.73, y: 2.3, z: myPlane });
+        entityEl.setAttribute('position', { x: 2.73, y: 2.3, z: myPlane_z });
         entityEl.setAttribute('rotation', { x: 0, y: -90, z: 0 });
         entityEl.setAttribute('color', 'white');
         entityEl.setAttribute('width', '1');
         entityEl.setAttribute('height', '1');
-        entityEl.setAttribute('clickimage', '');
-        /*
-        entityEl.setAttribute('animation', {
-          attribute: 'scale', 
-          dur:500, 
-          direction:'alternate', 
-          from:'1 1 1', 
-          to:'3 3 3', 
-          begin:'click', 
-          repeat:1});*/
+        //entityEl.setAttribute('clickimage', '');
         sceneEl.appendChild(entityEl);
-
         //right lights
         var entityEl = document.createElement('a-entity');
         entityEl.setAttribute('gltf-model', "url(assets/images/gltf/wallight/wallight.gltf)");
-        entityEl.setAttribute('position', { x: 2.55, y: 2.9, z: myPlane });
+        entityEl.setAttribute('position', { x: 2.55, y: 2.9, z: myPlane_z });
         entityEl.setAttribute('rotation', { x: 0, y: -90, z: 0 });
         entityEl.setAttribute('scale', { x: 0.005, y: 0.005, z: 0.005 });
         sceneEl.appendChild(entityEl);
     }
-    return myPlane
-}
+    myPlane_z = myPlane_z - 2;
+    //return myPlane
+    }
 
-function myEndWall(closingDoor) {
+function myEndWall(left_wall_z) {
     var sceneEl = document.querySelector('a-scene');
     var entityEl = document.createElement('a-plane');
     entityEl.setAttribute('material', { opacity: 0.4, transparent: "true", side: "double" });
-    entityEl.setAttribute('position', { x: -1.8, y: 2.45, z: closingDoor });
+    entityEl.setAttribute('position', { x: -1.8, y: 2.45, z: left_wall_z });
     entityEl.setAttribute('width', '2');
     entityEl.setAttribute('height', '4.12');
     entityEl.setAttribute('depth', '1');
@@ -254,7 +246,7 @@ function myEndWall(closingDoor) {
 
     var entityEl = document.createElement('a-plane');
     entityEl.setAttribute('material', { opacity: 0.4, transparent: "true", side: "double" });
-    entityEl.setAttribute('position', { x: 1.8, y: 2.45, z: closingDoor });
+    entityEl.setAttribute('position', { x: 1.8, y: 2.45, z: left_wall_z });
     entityEl.setAttribute('width', '2');
     entityEl.setAttribute('height', '4.12');
     entityEl.setAttribute('depth', '1');
@@ -263,7 +255,7 @@ function myEndWall(closingDoor) {
 
     var entityEl = document.createElement('a-plane');
     entityEl.setAttribute('material', { opacity: 0.4, transparent: "true", side: "double" });
-    entityEl.setAttribute('position', { x: 0, y: 3.7, z: closingDoor });
+    entityEl.setAttribute('position', { x: 0, y: 3.7, z: left_wall_z });
     entityEl.setAttribute('width', '1.6');
     entityEl.setAttribute('height', '1.6');
     entityEl.setAttribute('depth', '1');
