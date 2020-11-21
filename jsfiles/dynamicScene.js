2e6f0352b0fe4340608fbb83229ca64371dd9d6d
ritvik-garg <ritvik.garg@students.iiit.ac.in>
2020-11-21 16:34:34 +0530

    api issue resolved

--- jsfiles/dynamicScene.js
+++ jsfiles/dynamicScene.js
@@ -21,8 +21,8 @@ AFRAME.registerComponent("clickimage", {
 AFRAME.registerComponent('foo', {
     init: function() {
         camera_left_x = -2.7, camera_right_x = 2.7, camera_forward_z = -7
-        //left_wall_x=-2.7,right_wall_x=2.7
-        left_wall_z=-7,right_wall_z=-7
+            //left_wall_x=-2.7,right_wall_x=2.7
+        left_wall_z = -7, right_wall_z = -7
         center_z = camera_forward_z / 2;
         myPlane = -9;
         closingDoor = -4;
@@ -38,9 +38,10 @@ AFRAME.registerComponent('rotation-reader', {
             // `position` is a three.js Vector3. this.el is element and object3D in 3js get the position/rotation which 
             //contains x,y,z
         myStartBeta = this.el.object3D.rotation.y;
+        // console.log("leftwall_z : " + left_wall_z)
         //console.log(myStartPosition_x,myStartPosition_z,myStartBeta,myLength)
         if (((myStartPosition_x < camera_right_x) && (myStartPosition_x > camera_left_x)) && (myStartPosition_z < left_wall_z)) {
-            
+            console.log("leftwall_z : " + left_wall_z);
             //camera_forward_z=camera_forward_z-6;
             // closingDoor=closingDoor-6;
             //myPlane = room(camera_forward_z,myPlane,closingDoor);
@@ -49,12 +50,13 @@ AFRAME.registerComponent('rotation-reader', {
     }
 });
 
-var flag = true;
-var beta, p1_x = 0;
-// var p1_y = 2.5;
-var p1_z = -10;
+
 
 function getValuesFromPathGen() {
+    var flag = true;
+    var beta, p1_x = 0;
+    // var p1_y = 2.5;
+    var p1_z = -10;
     if (flag == true) {
         beta = 3.14; // 135 deg
         //beta = 2.35619; // 135 deg
@@ -75,7 +77,7 @@ function getValuesFromPathGen() {
         p2_beta = apiJson.new_beta;
         p2_x = apiJson.new_point[0];
         p2_z = apiJson.new_point[1];
-        console.log(p2_x, p2_z, p2_beta);
+        console.log("p2 points : " + p2_x, p2_z, p2_beta);
         create_room(p1_x, p1_z, p2_x, p2_z, beta, flag);
         p1_x = p2_x;
         p1_z = p2_z;
@@ -89,6 +91,7 @@ var room_height = 5;
 var room_depth = 6;
 
 function create_room(p1_x, p1_z, p2_x, p2_z, beta, flag) {
+    console.log("rom parameter : " + p1_x, p1_z, p2_x, p2_z, beta, flag)
     var room_centre_x = (p1_x + p2_x) / 2;
     var room_centre_z = (p1_z + p2_z) / 2;
     var beta_degree;
@@ -103,20 +106,20 @@ function create_room(p1_x, p1_z, p2_x, p2_z, beta, flag) {
     left_wall_z = room_centre_z;
     right_wall_z = room_centre_z;
 
-    room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room_centre_x,room_centre_z)
-    //left_wall
-    // create_wall(left_wall_x, left_wall_z, beta_degree);
-    //right wall
-    // create_wall(right_wall_x, right_wall_z, beta_degree);
+    room(left_wall_x, right_wall_x, left_wall_z, right_wall_z, beta_degree, room_centre_x, room_centre_z)
+        //left_wall
+        // create_wall(left_wall_x, left_wall_z, beta_degree);
+        //right wall
+        // create_wall(right_wall_x, right_wall_z, beta_degree);
 
     // similarly 3 planes on left wall and 3 on right wall
 }
 
-function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room_centre_x,room_centre_z) {
-    beta_degree=180
-    console.log("leftwallx "+left_wall_x)
-    console.log("zis "+left_wall_z)
-    console.log("beta is "+beta_degree)
+function room(left_wall_x, right_wall_x, left_wall_z, right_wall_z, beta_degree, room_centre_x, room_centre_z) {
+    beta_degree = 180
+    console.log("leftwallx " + left_wall_x)
+    console.log("z is " + left_wall_z)
+    console.log("beta is " + beta_degree)
     var sceneEl = document.querySelector('a-scene');
     //right wall
     var entityEl = document.createElement('a-box');
@@ -161,7 +164,7 @@ function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room
     // //chandeleur 
     var entityEl = document.createElement('a-entity');
     entityEl.setAttribute('gltf-model', "url(assets/images/gltf/chandeleur/chandeleur.gltf)");
-    entityEl.setAttribute('position', { x: 0, y: 3, z: left_wall_z });
+    entityEl.setAttribute('position', { x: 0, y: 3, z: room_centre_z });
     entityEl.setAttribute('rotation', { x: 360, y: 0, z: 0 });
     entityEl.setAttribute('scale', { x: 0.04, y: 0.04, z: 0.04 });
     sceneEl.appendChild(entityEl);
@@ -180,10 +183,10 @@ function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room
     entityEl2.setAttribute('position', { x: 2.55, y: 0.6, z: left_wall_z });
     sceneEl.appendChild(entityEl2);
 
-    myEndWall(left_wall_z)
-    myEndWall(left_wall_z - 6)
+    // myEndWall(left_wall_z)
+    // myEndWall(left_wall_z - 6)
 
-    myPlane_z = left_wall_z+2
+    myPlane_z = left_wall_z + 2
     for (j = 0; j < 3; j++) {
 
         //myPlane = myPlane - 2;
@@ -209,7 +212,7 @@ function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room
         entityEl.setAttribute('scale', { x: 0.005, y: 0.005, z: 0.005 });
         sceneEl.appendChild(entityEl);
 
-    //     //right planes
+        //     //right planes
         var entityEl = document.createElement('a-plane');
         painting_no = Math.floor(Math.random() * 6);
         var path = `assets/images/paint${painting_no}.jpg`;
@@ -228,10 +231,12 @@ function room(left_wall_x,right_wall_x,left_wall_z,right_wall_z,beta_degree,room
         entityEl.setAttribute('rotation', { x: 0, y: -90, z: 0 });
         entityEl.setAttribute('scale', { x: 0.005, y: 0.005, z: 0.005 });
         sceneEl.appendChild(entityEl);
+
+        myPlane_z = myPlane_z - 2;
     }
-    myPlane_z = myPlane_z - 2;
-    //return myPlane
-    }
+    // myPlane_z = myPlane_z - 2;
+    // return myPlane
+}
 
 function myEndWall(left_wall_z) {
     var sceneEl = document.querySelector('a-scene');
